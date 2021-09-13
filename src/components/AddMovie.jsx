d9be7d463626ab78cfb4b58e312d8c0d9ed5e488
Christian Volz <christianbvolz@gmail.com>
2021-09-13 06:36:46 -0300

    requisito 10

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -48,6 +48,17 @@ class AddMovie extends React.Component {
             onChange={ this.handleChange }
           />
         </label>
+        <label htmlFor="image-input" data-testid="image-input-label">
+          Imagem
+          <input
+            type="text"
+            value={ imagePath }
+            name="imagePath"
+            data-testid="image-input"
+            id="image-input"
+            onChange={ this.handleChange }
+          />
+        </label>
       </form>
     );
   }
