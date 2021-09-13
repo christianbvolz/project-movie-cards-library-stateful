15685545ab0aa09b433d57a801674f4483276535
Christian Volz <christianbvolz@gmail.com>
2021-09-13 06:33:15 -0300

    requisito 8

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -11,13 +11,31 @@ class AddMovie extends React.Component {
       rating: 0,
       genre: 'action',
     };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  // handleChange = ({ value, name }) => {
+  //   this.setState({ [name]: value });
+  // }
+  handleChange({ target }) {
+    const { name, value } = target;
+    this.setState({ [name]: value });
   }
 
   render() {
+    const { title, subtitle, imagePath, storyLine, rating, genre } = this.state;
     return (
       <form data-testid="add-movie-form">
-        <label>
-
+        <label htmlFor="title-input" data-testid="title-input-label">
+          Título
+          <input
+            type="text"
+            value={ title }
+            name="title"
+            data-testid="title-input"
+            id="title-input"
+            onChange={ this.handleChange }
+          />
         </label>
       </form>
     );
