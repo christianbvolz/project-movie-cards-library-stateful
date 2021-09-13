e94539c3b0fd83513eb1ef7c11135b67ed086b23
Christian Volz <christianbvolz@gmail.com>
2021-09-13 06:18:53 -0300

    requisito 6

--- src/components/AddMovie.jsx
+++ src/components/AddMovie.jsx
@@ -1,1 +1,23 @@
-// implement AddMovie component here
+import React from 'react';
+
+class AddMovie extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      subtitle: '',
+      title: '',
+      imagePath: '',
+      storyLine: '',
+      rating: 0,
+      genre: 'action',
+    };
+  }
+
+  render() {
+    return (
+      <h1>Roi...</h1>
+    );
+  }
+}
+
+export default AddMovie;
