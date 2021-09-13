a0c247174d2966256df2b7a91e2555e6c208b712
Christian Volz <christianbvolz@gmail.com>
2021-09-13 09:10:06 -0300

    requisito 15

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
-import SearchBar from './components/SearchBar';
+
 import './App.css';
-import AddMovie from './components/AddMovie';
+import MovieLibrary from './components/MovieLibrary';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <SearchBar
-        searchText=""
-        onSearchTextChange=""
-        bookmarkedOnly=""
-        onBookmarkedChange=""
-        selectedGenre=""
-        onSelectedGenreChange=""
-      />
-      <AddMovie />
+      <MovieLibrary />
     </div>
   );
 }
--- src/components/MovieLibrary.jsx
+++ src/components/MovieLibrary.jsx
@@ -1,1 +1,24 @@
-// implement MovieLibrary component here
+import React from 'react';
+import AddMovie from './AddMovie';
+import SearchBar from './SearchBar';
+
+class MovieLibrary extends React.Component {
+  render() {
+    const { movies } = this.props;
+    return (
+      <>
+        <SearchBar
+          searchText=""
+          onSearchTextChange=""
+          bookmarkedOnly=""
+          onBookmarkedChange=""
+          selectedGenre=""
+          onSelectedGenreChange=""
+        />
+        <AddMovie />
+      </>
+    );
+  }
+}
+
+export default MovieLibrary;
